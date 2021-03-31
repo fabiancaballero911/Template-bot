bfc254ea20090ed3a42e4db223557ef539a2cbce
fabiancaballero911 <76545284+fabiancaballero911@users.noreply.github.com>
2021-03-30 23:12:48 -0500

    Update bot.js

--- commands/test/bot.js
+++ commands/test/bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 
 module.exports = {
-    name: "comando",
-    description: "descri",
+    name: "comando",//aca el nombre del comando
+    description: "descri",//la descripción del comando
 
     async run (client, message, args) {
 //code qe se ejecutara al poner el comando con su prefix
